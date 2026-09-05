1e12817d74783dc13f26375d003300d8b5a00d38
RealHinome <RealHinome@users.noreply.github.com>
2026-09-05 14:08:18 +0000

    deploy: d61fa490d35e517e2ea2097ffa105bfae46839f5

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-93168774.js',
+                path_to_root + 'searchindex-0388ebe5.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
